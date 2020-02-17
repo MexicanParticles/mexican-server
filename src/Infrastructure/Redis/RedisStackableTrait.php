fe99127b63719539816b76c7f8df97425f28a288
kono-t <41985786+kono-t@users.noreply.github.com>
2020-02-17 21:24:27 +0900

    add redis stackable trait

---
+++ src/Infrastructure/Redis/RedisStackableTrait.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Redis;
+
+use JsonSerializable;
+use Redis;
+use UnexpectedValueException;
+
+/**
+ * Behave as stack using the list of Redis.
+ * @package App\Infrastructure\Redis
+ * @see https://github.com/phpredis/phpredis#sorted-sets
+ * @property Redis $redis
+ */
+trait RedisStackableTrait
+{
+    /**
+     * @param iterable<string|JsonSerializable> $iterable
+     * @param string                            $key
+     * @throws RedisKeyAlreadyExistsException|UnexpectedValueException
+     */
+    public function setAsStack(iterable $iterable, string $key): void
+    {
+        if (false === $this->isEmpty($key)) {
+            throw new RedisKeyAlreadyExistsException('key: %s already exists', $key);
+        }
+
+        foreach ($iterable as $element) {
+            if ($element instanceof JsonSerializable) {
+                $value = $element->jsonSerialize();
+            } elseif (is_string($element)) {
+                $value = $element;
+            } else {
+                throw new UnexpectedValueException('Element to be stacked must be string or jsonSerializable');
+            }
+
+            $this->redis->lPush($key, (string) $value);
+        }
+    }
+
+    /**
+     * @param string $key
+     * @return string
+     * @throws RedisKeyNotExistsException
+     */
+    public function pop(string $key): string
+    {
+        $popped = $this->redis->lPop($key);
+        if (false === $popped) {
+            throw new RedisKeyNotExistsException('key: %s not exists', $key);
+        }
+        assert(is_string($popped));
+        return $popped;
+    }
+
+    /**
+     * @param string $key
+     * @return int
+     */
+    public function count(string $key): int
+    {
+        return $this->redis->lLen($key);
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function isEmpty(string $key): bool
+    {
+        return 0 === $this->count($key);
+    }
+}
---
+++ tests/Infrastructure/Redis/Test.php
@@ -0,0 +1,106 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Infrastructure\Redis;
+
+use App\Infrastructure\Redis\RedisStackableTrait;
+use JsonSerializable;
+use Redis;
+use Tests\TestCase;
+
+/**
+ * @package Tests\Infrastructure\Redis
+ * @TODO add tests for exceptions
+ */
+class RedisStackableTest extends TestCase
+{
+    private $redis;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $app = $this->getAppInstance();
+        $redis = $app->getContainer()->get(Redis::class);
+        $this->redis = new class($redis) {
+            use RedisStackableTrait;
+            private $redis;
+
+            public function __construct(Redis $redis)
+            {
+                $this->redis = $redis;
+            }
+        };
+    }
+
+    public function testPublicMethodsWithArrayWithString(): void
+    {
+        $stack = ['hoge', 'fuga', 'piyo'];
+        $this->redis->setAsStack($stack, $key = 'mexico');
+
+        self::assertFalse($this->redis->isEmpty($key));
+        self::assertSame(3, $this->redis->count($key));
+        self::assertSame('piyo', $this->redis->pop($key));
+
+        self::assertFalse($this->redis->isEmpty($key));
+        self::assertSame(2, $this->redis->count($key));
+        self::assertSame('fuga', $this->redis->pop($key));
+
+        self::assertFalse($this->redis->isEmpty($key));
+        self::assertSame(1, $this->redis->count($key));
+        self::assertSame('hoge', $this->redis->pop($key));
+
+        self::assertTrue($this->redis->isEmpty($key));
+        self::assertSame(0, $this->redis->count($key));
+    }
+
+    public function testPublicMethodsWithCollectionWithJsonSerializable(): void
+    {
+        $jsonSerializableA = new class() implements JsonSerializable {
+            /**
+             * @return string
+             */
+            public function jsonSerialize(): string
+            {
+                return 'hogehoge';
+            }
+        };
+        $jsonSerializableB = new class() implements JsonSerializable {
+            /**
+             * @return string
+             */
+            public function jsonSerialize(): string
+            {
+                return 'fugafuga';
+            }
+        };
+        $jsonSerializableC = new class() implements JsonSerializable {
+            /**
+             * @return string
+             */
+            public function jsonSerialize(): string
+            {
+                return 'piyopiyo';
+            }
+        };
+
+        $stack = collect([$jsonSerializableA, $jsonSerializableB, $jsonSerializableC]);
+        $this->redis->setAsStack($stack, $key = 'mexico');
+
+        self::assertFalse($this->redis->isEmpty($key));
+        self::assertSame(3, $this->redis->count($key));
+        self::assertSame('piyopiyo', $this->redis->pop($key));
+
+        self::assertFalse($this->redis->isEmpty($key));
+        self::assertSame(2, $this->redis->count($key));
+        self::assertSame('fugafuga', $this->redis->pop($key));
+
+        self::assertFalse($this->redis->isEmpty($key));
+        self::assertSame(1, $this->redis->count($key));
+        self::assertSame('hogehoge', $this->redis->pop($key));
+
+        self::assertTrue($this->redis->isEmpty($key));
+        self::assertSame(0, $this->redis->count($key));
+    }
+}
